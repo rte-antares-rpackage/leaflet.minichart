cb173f2a47f2ee0514338cbda2717836ee075b4a
françois GUILLEM <francois.guillem@rte-france.com>
2017-01-12 16:06:40 +0100

    Add licence

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+// Copyright © 2016 RTE Réseau de transport d’électricité
+
 module.exports = function(grunt) {
 
   // Project configuration.
--- src/d3chart.js
+++ src/d3chart.js
@@ -1,3 +1,5 @@
+// Copyright © 2016 RTE Réseau de transport d’électricité
+
 var d3 = require("d3");
 var tinycolor = require("tinycolor2");
 
